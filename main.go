2cff45875bf375851eb7dfe60eadb0bf72b0640c
alonlong <alonlong@163.com>
2021-01-02 10:35:46 +0800

    support config file for application

--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"soci-avatar-cdn/config"
 	"soci-avatar-cdn/route"
 )
 
-func setupRoutes() {
+func setupRoutes(settings *config.Config) {
 	http.Handle("/", http.FileServer(http.Dir("./files")))
 	http.HandleFunc("/upload", route.UploadFile)
 
 	port := os.Getenv("APP_PORT")
 	if port == "" {
-		port = "4202"
+		port = settings.Port
+		if port == "" {
+			port = "4202"
+		}
 	}
 
 	fmt.Printf("Listening on %v\n", port)
@@ -21,6 +25,16 @@ func setupRoutes() {
 }
 
 func main() {
+	var settings config.Config
+	// parse the config file
+	if err := config.ParseJSONFile("./config.json", &settings); err != nil {
+		panic(err)
+	}
+	// validate the config file
+	if err := settings.Validate(); err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting avatar encoding server...")
-	setupRoutes()
+	setupRoutes(&settings)
 }
